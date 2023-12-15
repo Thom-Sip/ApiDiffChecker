abbc498a5925012cd7881efd56209b361f3ead5e
Thom Sip <info@thomsip.net>
2023-12-15 16:51:50 +0100

    start params

--- src/RefactorHelper.Models/RequestDetails.cs
+++ src/RefactorHelper.Models/RequestDetails.cs
@@ -4,6 +4,8 @@ namespace RefactorHelper.Models
 {
     public class RequestDetails
     {
+        public required string Template { get; init; }
+
         public required string Path { get; init; }
 
         public required Operation Operation { get; init; }
--- src/RefactorHelper.SwaggerProcessor/SwaggerProcessorService.cs
+++ src/RefactorHelper.SwaggerProcessor/SwaggerProcessorService.cs
@@ -1,11 +1,17 @@
 ﻿using Newtonsoft.Json;
 using RefactorHelper.Models;
 using Swashbuckle.Swagger;
+using System.Text.RegularExpressions;
 
 namespace RefactorHelper.SwaggerProcessor
 {
     public class SwaggerProcessorService
     {
+        private Dictionary<string, string> Variables = new Dictionary<string, string>
+        {
+            { "customerid", "1" }
+        };
+
         public List<RequestDetails> ProcessSwagger(string swaggerJson)
         {
             var doc = JsonConvert.DeserializeObject<SwaggerDocument>(swaggerJson);
@@ -18,12 +24,31 @@ namespace RefactorHelper.SwaggerProcessor
             {
                 return new RequestDetails 
                 { 
-                    Path = path.Key,
+                    Template = path.Key.ToLower(),
+                    Path = GetPath(path.Key.ToLower(), path.Value.get),
                     Operation = path.Value.get,
                 };
             }
 
             throw new NotImplementedException();
+        }
+
+        private string GetPath(string template, Operation operation)
+        {
+            Match match = Regex.Match(template, @"\{(.+?)\}");
+
+            if(match.Success)
+            {
+                if (Variables.ContainsKey(GetParam(match.Value)))
+                    template = template.Replace(match.Value, Variables[GetParam(match.Value)]);
+            }
+
+            return template;
+        }
+
+        private string GetParam(string urlPart)
+        {
+            return urlPart.Replace("{", "").Replace("}", "");
         }
     }
 }
