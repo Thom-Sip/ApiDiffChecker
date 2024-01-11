47cfa10c918449ad11b06dfc4b084a13d5072839
Thom Sip <info@thomsip.net>
2024-01-11 16:23:38 +0100

    Remove old files

--- src/RefactorHelper.Models/Config/Parameter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RefactorHelper.Models.Config
-{
-    public class Parameter(string key, string value)
-    {
-        public string Key { get; private set; } = key;
-
-        public string Value { get; set; } = value;
-    }
-}
