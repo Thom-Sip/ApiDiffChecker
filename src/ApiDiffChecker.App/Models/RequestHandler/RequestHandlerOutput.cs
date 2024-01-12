c2b296ea518e4fb3e94614c52188b92eaf65e23c
Thom Sip <info@thomsip.net>
2024-01-12 01:56:49 +0100

    remove unused file

--- src/ApiDiffChecker.App/Models/RequestHandler/RequestHandlerOutput.cs
+++
@@ -1,9 +0,0 @@
-﻿using ApiDiffChecker.Models.RequestHandler;
-
-namespace ApiDiffChecker.Models
-{
-    public class RequestHandlerOutput
-    {
-        public List<RefactorTestResultPair> Results { get; init; } = [];
-    }
-}
